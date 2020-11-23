497d876a12d25dc99f049f9b845d2a4f230a4306
Blue Sky <3240418931@qq.com>
2020-11-23 16:09:20 +0800

    12123

--- resource.h
+++ resource.h
@@ -7,6 +7,7 @@
 #define IDS_ABOUTBOX                    101
 #define IDD_MFC_DIALOG                  102
 #define IDR_MAINFRAME                   128
+#define IDD_DIALOG1                     130
 #define IDD_HOME_GIF                    130
 #define IDD_PANEL_DIG                   137
 #define IDD_PANEL_Dlg                   137
@@ -50,6 +51,7 @@
 //
 #ifdef APSTUDIO_INVOKED
 #ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NEXT_RESOURCE_VALUE        132
 #define _APS_NEXT_RESOURCE_VALUE        147
 #define _APS_NEXT_COMMAND_VALUE         32771
 #define _APS_NEXT_CONTROL_VALUE         1038
